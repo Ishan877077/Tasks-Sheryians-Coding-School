6c0ef0538896a2d8fad940a186908c84d6af8bf5
Ishan877077 <bhawsarishan567@gmail.com>
2026-09-14 01:09:37 +0530

    Initialize project and add images folder

---
+++ script.js
@@ -0,0 +1,64 @@
+gsap.to("#section_2_child_id_13>img", {
+    Y:-20,
+    duration:3,
+    delay:1,
+    rotate:7,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("#section_2_child_id_14>img", {
+    x:-20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("#section_2_child_id_15>img", {
+    x:-20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("#section_2_child_id_16>img", {
+    x:-20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("#section_2_child_id_17>img", {
+    x: -20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("#section_2_child_id_18>img", {
+    x: 20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("#section_2_child_id_19>img", {
+    x: 20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,   
+})
+
+gsap.to("h1", {
+    x: -20,
+    duration:3,
+    delay:1,
+    repeat:-1,
+    yoyo:true,
+})
